c88d4226903dba6f92713ec2d4502180701e2768
Muhammed-Anees-P <aneespengad4447@gmail.com>
2024-12-21 16:49:47 +0530

    import react router and setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
+import WatchHistory from "./pages/WatchHistory";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Video Player</h1>
+      <Routes>
+        <Route path={"/"} element={<LandingPage />} />
+        <Route path={"/home"} element={ <HomePage />} />
+        <Route path={"/watch-history"} element={ <WatchHistory />} />
+
+       
+       
+      </Routes>
     </div>
   );
 }
