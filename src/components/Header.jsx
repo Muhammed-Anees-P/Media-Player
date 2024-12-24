d939dc82208b8bac0122638e92177ad968aad26d
Muhammed-Anees-P <aneespengad4447@gmail.com>
2024-12-24 15:23:15 +0530

    header modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <Navbar className="bg-body-tertiary bg-dark">
-        <Container>
-          <Navbar.Brand href="#home">
-          <i class="fa-solid fa-video fa-bounce text-warning"></i>{' '}
-            <span className='text-white'>Video Player</span>
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-  )
+      <Container>
+        <Navbar.Brand href="#home">
+          <Link to={'/'} style={{textDecoration:"none"}}>
+            <i class="fa-solid fa-video fa-bounce text-warning"></i>{" "}
+            <span className="text-white">Video Player</span>
+          </Link>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
