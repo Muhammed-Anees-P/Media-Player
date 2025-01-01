b7ee337738870b78de4525cbb0c88647da4704a3
Muhammed-Anees-P <aneespengad4447@gmail.com>
2025-01-01 10:58:02 +0530

    videoCard.jsx file updated

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Card } from "react-bootstrap";
 
 function VideoCard() {
   return (
-    <div>VideoCard</div>
-  )
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="https://picsum.photos/200/300" />
+        <Card.Body>
+          <Card.Title className="d-flex justify-content-between align-items-center">
+            <h6>Video Caption</h6>
+            <button className="btn btn-danger">
+              <i class="fa-solid fa-trash-can"></i>{" "}
+            </button>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
-export default VideoCard
+export default VideoCard;
